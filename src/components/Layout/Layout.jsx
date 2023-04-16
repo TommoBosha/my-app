eac730b5569c5a612f1ac1d3824364e79c892805
TommoBosha <116563699+TommoBosha@users.noreply.github.com>
2023-04-16 23:06:18 +0300

    add test

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,7 +1,7 @@
 import { Suspense } from "react";
 // import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
-import useLogin from "../../hooks/useLogin";
+// import useLogin from "../../hooks/useLogin";
 // import { IsUserLogin } from "../../redux/auth/authSelectors";
 // import AuthForm from "../AuthForm/AuthForm";
 import Loader from "../Loader/Loader";
@@ -9,21 +9,20 @@ import Navigation from "../Navigation/Navigation";
 // import UserMenu from "../Navigation/UserMenu/UserMenu";
 
 const Layout = () => {
-    const isLogin = useLogin();
-    
-    return (
-        <div>
-            <header>
-                <Navigation />
-                {/* {isLogin && <UserMenu />} */}
-                {/* : <AuthForm/> */}
-            </header>
-            <Suspense fallback={<Loader />}>
-                <Outlet/>
-            </Suspense>
-        </div>
-    )
-    
+  // const isLogin = useLogin();
+
+  return (
+    <div>
+      <header>
+        <Navigation />
+        {/* {isLogin && <UserMenu />} */}
+        {/* : <AuthForm/> */}
+      </header>
+      <Suspense fallback={<Loader />}>
+        <Outlet />
+      </Suspense>
+    </div>
+  );
 };
 
 export default Layout;
